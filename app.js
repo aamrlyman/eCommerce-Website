6b0a6e6a12ad1f9190daf79b69221da63d82c351
aamrlyman <aamrlyman@gmail.com>
2022-09-16 08:06:23 -0700

    Fix home image and header

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// When the user scrolls the page, execute myFunction
+window.addEventListener("load", function () {
+
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("myHeader");
+
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+
+});
